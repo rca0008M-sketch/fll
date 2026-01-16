ca465c968190b051afe6edefd508e457c8b324a7
yasser amrani <amrani67.yasser41@gmail.com>
2026-01-16 21:49:38 +0100

    Add asset_map files

---
+++ asset_map/asset_map.js
@@ -0,0 +1,220 @@
+// Asset Map (Read-Only)
+// Edit assets in the Media tab.
+// Use these IDs alongside lib to reference assets in your code:
+//
+// Example: lib.getAsset["player_sprite"]
+
+const assets = {
+  "main_menu_background": {
+    "url": "https://d2oir5eh8rty2e.cloudfront.net/assets/images/98f32e39-4e30-4666-8a32-fc5a550d3921.webp",
+    "type": "image",
+    "aspect_ratio": [
+      1024,
+      1536
+    ]
+  },
+  "roadmap_background": {
+    "url": "https://d2oir5eh8rty2e.cloudfront.net/assets/images/f51cd4c6-09cc-4323-a324-5c2958661bb1.webp",
+    "type": "image",
+    "aspect_ratio": [
+      1024,
+      1536
+    ]
+  },
+  "jebel_irhoud_site_icon": {
+    "url": "https://d2oir5eh8rty2e.cloudfront.net/assets/images/a5026ab1-ab1a-4a68-913e-e90877b90abe.webp",
+    "type": "image",
+    "aspect_ratio": [
+      854,
+      881
+    ]
+  },
+  "volubilis_site_icon": {
+    "url": "https://d2oir5eh8rty2e.cloudfront.net/assets/images/82910796-256a-42d1-b441-e8f674ba175b.webp",
+    "type": "image",
+    "aspect_ratio": [
+      934,
+      936
+    ]
+  },
+  "chellah_site_icon": {
+    "url": "https://d2oir5eh8rty2e.cloudfront.net/assets/images/ffcaf3f4-8278-4181-b1a3-bc583a0e9be9.webp",
+    "type": "image",
+    "aspect_ratio": [
+      902,
+      933
+    ]
+  },
+  "artifact_card_jebel_irhoud_skull": {
+    "url": "https://d2oir5eh8rty2e.cloudfront.net/assets/images/e576f7cf-e55b-4653-a79d-103ae623467e.webp",
+    "type": "image",
+    "aspect_ratio": [
+      1024,
+      1024
+    ]
+  },
+  "artifact_card_stone_tool": {
+    "url": "https://d2oir5eh8rty2e.cloudfront.net/assets/images/7b36286f-ab0f-45d2-bfc5-a1a05fafd619.webp",
+    "type": "image",
+    "aspect_ratio": [
+      1024,
+      1024
+    ]
+  },
+  "artifact_card_roman_mosaic": {
+    "url": "https://d2oir5eh8rty2e.cloudfront.net/assets/images/60d5e46b-5883-45c6-b5db-32fb2a9e6594.webp",
+    "type": "image",
+    "aspect_ratio": [
+      1024,
+      1024
+    ]
+  },
+  "artifact_card_oil_lamp": {
+    "url": "https://d2oir5eh8rty2e.cloudfront.net/assets/images/9ce173da-92dc-4f87-9846-8bf383dfc9cf.webp",
+    "type": "image",
+    "aspect_ratio": [
+      1024,
+      1024
+    ]
+  },
+  "artifact_card_ceramic_bowl": {
+    "url": "https://d2oir5eh8rty2e.cloudfront.net/assets/images/b06d0e25-3680-440e-911f-ef7ad7d6f364.webp",
+    "type": "image",
+    "aspect_ratio": [
+      1024,
+      1024
+    ]
+  },
+  "artifact_card_ancient_coin": {
+    "url": "https://d2oir5eh8rty2e.cloudfront.net/assets/images/60c7116a-7a76-4edc-87eb-a4a9f98e83a0.webp",
+    "type": "image",
+    "aspect_ratio": [
+      1024,
+      1024
+    ]
+  },
+  "tool_brush": {
+    "url": "https://d2oir5eh8rty2e.cloudfront.net/assets/images/e20c2b93-f978-40a6-86b6-292ed690013f.webp",
+    "type": "image",
+    "aspect_ratio": [
+      763,
+      897
+    ]
+  },
+  "tool_measuring_tape": {
+    "url": "https://d2oir5eh8rty2e.cloudfront.net/assets/images/be7cb476-56db-456b-85b1-04e1e4466a58.webp",
+    "type": "image",
+    "aspect_ratio": [
+      786,
+      692
+    ]
+  },
+  "tool_magnifying_glass": {
+    "url": "https://d2oir5eh8rty2e.cloudfront.net/assets/images/e842e468-3bd8-4add-8f3e-95b61a9fea44.webp",
+    "type": "image",
+    "aspect_ratio": [
+      932,
+      928
+    ]
+  },
+  "tool_hammer": {
+    "url": "https://d2oir5eh8rty2e.cloudfront.net/assets/images/d8aea0e4-86c5-4283-83a0-2cd31e84f053.webp",
+    "type": "image",
+    "aspect_ratio": [
+      658,
+      876
+    ]
+  },
+  "tool_shovel": {
+    "url": "https://d2oir5eh8rty2e.cloudfront.net/assets/images/eb8f78c7-f683-4ad5-90f3-28dbca769cc1.webp",
+    "type": "image",
+    "aspect_ratio": [
+      778,
+      999
+    ]
+  },
+  "wrong_answer_dinosaur_egg": {
+    "url": "https://d2oir5eh8rty2e.cloudfront.net/assets/images/34cf1e33-706f-40e8-94e7-332d9be10c2f.webp",
+    "type": "image",
+    "aspect_ratio": [
+      756,
+      725
+    ]
+  },
+  "wrong_answer_treasure_chest": {
+    "url": "https://d2oir5eh8rty2e.cloudfront.net/assets/images/f44db6f7-c32f-4f9a-b181-d923061c646c.webp",
+    "type": "image",
+    "aspect_ratio": [
+      783,
+      788
+    ]
+  },
+  "star_icon": {
+    "url": "https://d2oir5eh8rty2e.cloudfront.net/assets/images/c1f61e45-06ec-4193-90ee-e69d3812fc8f.webp",
+    "type": "image",
+    "aspect_ratio": [
+      861,
+      810
+    ]
+  },
+  "lock_icon": {
+    "url": "https://d2oir5eh8rty2e.cloudfront.net/assets/images/7cd74856-8cde-4127-b293-02821956cf7c.webp",
+    "type": "image",
+    "aspect_ratio": [
+      621,
+      858
+    ]
+  },
+  "correct_answer_celebration": {
+    "url": "https://d2oir5eh8rty2e.cloudfront.net/assets/images/4c1cda99-997b-47c1-a899-b7ff4dd8ecda.webp",
+    "type": "image",
+    "aspect_ratio": [
+      860,
+      866
+    ]
+  },
+  "question_background": {
+    "url": "https://d2oir5eh8rty2e.cloudfront.net/assets/images/30deab8d-0df2-421b-8943-717ca7cf520e.webp",
+    "type": "image",
+    "aspect_ratio": [
+      1024,
+      1536
+    ]
+  },
+  "collection_book_background": {
+    "url": "https://d2oir5eh8rty2e.cloudfront.net/assets/images/c87b5115-c105-42fc-a7f5-1a9f58c0f7c1.webp",
+    "type": "image",
+    "aspect_ratio": [
+      1024,
+      1536
+    ]
+  },
+  "background_music_main": {
+    "url": "https://d2oir5eh8rty2e.cloudfront.net/assets/sounds/music/4aa9c6df-13a0-4c94-851b-77a1799527c8.mp3",
+    "type": "audio"
+  },
+  "sfx_correct_answer": {
+    "url": "https://d2oir5eh8rty2e.cloudfront.net/assets/sounds/effect/d73247dd-b8bb-4d50-90c8-ae89c028ae3b.mp3",
+    "type": "audio"
+  },
+  "sfx_incorrect_answer": {
+    "url": "https://d2oir5eh8rty2e.cloudfront.net/assets/sounds/effect/d2b09a2c-20a4-48be-8d18-f2d1989b40e8.mp3",
+    "type": "audio"
+  },
+  "sfx_button_click": {
+    "url": "https://d2oir5eh8rty2e.cloudfront.net/assets/sounds/effect/b485947f-ff20-457f-915a-8d764509ea2e.mp3",
+    "type": "audio"
+  },
+  "sfx_star_earned": {
+    "url": "https://d2oir5eh8rty2e.cloudfront.net/assets/sounds/effect/d3b34a1a-d913-4128-907a-c3c952e29d8e.mp3",
+    "type": "audio"
+  },
+  "sfx_site_unlock": {
+    "url": "https://d2oir5eh8rty2e.cloudfront.net/assets/sounds/effect/c842bac7-e42a-44f7-9459-7881b4a1b69e.mp3",
+    "type": "audio"
+  },
+  "sfx_card_collect": {
+    "url": "https://d2oir5eh8rty2e.cloudfront.net/assets/sounds/effect/b5451467-0b08-42ef-b844-364de31483a7.mp3",
+    "type": "audio"
+  }
+};
